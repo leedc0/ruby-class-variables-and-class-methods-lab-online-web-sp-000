816e8cffbe9d4aa96059b18c20c4ad1b15baede5
Daniel Lee <leedc@me.com>
2019-10-09 21:07:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,17 @@
 class Song
 
-attr_accessor :name, :artist, :genre
+# attr_accessor :name, :artist, :genre
+@@count = 0
+@@genre = []
+
+def initialize
+    @@count += 1
+  end
 
 def new(name, artist, genre)
 
 end
+
+def name()
 
 end
