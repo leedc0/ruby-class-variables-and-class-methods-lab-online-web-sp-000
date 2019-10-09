0018b5c49d5fabeea51c2e84177ac3b746d2e0fa
Daniel Lee <leedc@me.com>
2019-10-09 22:01:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,7 @@ attr_accessor :name, :artist, :genre
     @genre = genre
 
     @@count += 1
+    @@artist << artist
   end
 
   def name
@@ -31,6 +32,8 @@ attr_accessor :name, :artist, :genre
     @@count
   end
 
-  
+  def artist.
+    @@artist
+  end
 
 end
